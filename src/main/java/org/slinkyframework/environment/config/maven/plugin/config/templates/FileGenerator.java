ce31dce09f664e43bc476adffd6b13131a052d5e
Keith Lyall <keith.lyall@gmail.com>
2018-11-01 18:39:49 +0000

    Migrated from slinky-environment

---
+++ src/main/java/org/slinkyframework/environment/config/maven/plugin/config/templates/FileGenerator.java
@@ -0,0 +1,7 @@
+package org.slinkyframework.environment.config.maven.plugin.config.templates;
+
+import java.io.File;
+
+public interface FileGenerator {
+    void generateFile(File templateFile);
+}
